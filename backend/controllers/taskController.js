c3c9283707bc9b2d7f4500f803f9dcf24214830e
Vindhuja BK <vindhuja.bk@gmail.com>
2024-04-21 16:10:24 +0530

    Initial backend server setup

---
+++ backend/controllers/taskController.js
@@ -0,0 +1,42 @@
+import taskModel from "../models/taskModel.js";
+import userModel from "../models/userModel.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+const addTask = async (req, res) => {
+  const { title, description } = req.body;
+  const userId = req.user.id;
+  const user = await userModel.find({ _id: userId });
+  const newTask = new taskModel({
+    title,
+    description,
+    completed: false,
+    userId,
+  });
+  newTask
+    .save()
+    .then(() => {
+      sendMail(user[0].email, "Task Added", title, description);
+      return res.status(200).json({ message: "Task added successfully" });
+    })
+    .catch((error) => {
+      return res.status(500).json({ message: error.message });
+    });
+};
+
+const removeTask = (req, res) => {
+  const { id } = req.body;
+  console.log("id: ", id);
+  taskModel
+    .findByIdAndDelete(id)
+    .then(() => res.status(200).json({ message: "Task deleted successfully" }))
+    .catch((error) => res.status(501).json({ message: error.message }));
+};
+
+const getTask = (req, res) => {
+  taskModel
+    .find({ userId: req.user.id })
+    .then((data) => res.status(200).json(data))
+    .catch((error) => res.status(501).json({ message: error.message }));
+};
+export { addTask, getTask, removeTask };
---
+++ backend/server.js
@@ -0,0 +1,34 @@
+import express from "express"
+import mongoose from "mongoose"
+import cors from "cors"
+import dotenv from "dotenv"
+
+import userRouter from "./routes/userRoute.js"
+import taskRouter from "./routes/taskRoute.js"
+
+//app config
+dotenv.config()
+const app = express()
+const port = process.env.PORT || 8001
+mongoose.set('strictQuery', true);
+
+//middlewares
+app.use(express.json())
+app.use(cors())
+
+//db config
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
+mongoose.connection.once('open', () => {
+    console.log("Connected to database");
+});
+mongoose.connection.on('error', (error) => {
+    console.error("Error connecting to database: ", error);
+});
+
+
+//api endpoints
+app.use("/api/user", userRouter)
+app.use("/api/task", taskRouter)
+
+//listen
+app.listen(port, () => console.log(`Listening on localhost:${port}`))
