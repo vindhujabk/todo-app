f5219dc1fc3eb8ad984e4e7970055b6f19a9cc90
Vindhuja BK <vindhuja.bk@gmail.com>
2024-04-25 11:17:12 +0530

    Implemented Gist upload feature

--- frontend/src/components/Task/Task.jsx
+++ frontend/src/components/Task/Task.jsx
@@ -6,11 +6,58 @@ import TaskContext from "../../context/TaskContext";
 import TokenContext from "../../context/TokenContext";
 import DeleteIcon from "@mui/icons-material/Delete";
 import axios from "../../Axios/axios.js"
+import UploadIcon from "@mui/icons-material/Upload";
+import Tooltip from "@mui/material/Tooltip";
+import { Octokit } from '@octokit/core';
+import dotenv from "dotenv"
 
 
-function Task({ task, id, todos }) {
+
+
+function Task({ task, id }) {
   const { dispatch } = useContext(TaskContext);
   const { userToken } = useContext(TokenContext);
+  const TOKEN = process.env.REACT_APP_GITHUB_TOKEN
+  console.log(TOKEN)
+
+  const octokit = new Octokit({
+    auth: 'TOKEN'
+  });
+
+  const uploadGist = async (e) => {
+    e.preventDefault();
+    try {
+      await createGist();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  async function createGist() {
+    try {
+      // Make a POST request to create a Gist
+      const response = await octokit.request('POST /gists', {
+        description: 'Example of a gist', // Description of the Gist
+        public: false, // Set to true if you want the Gist to be public
+        files: {
+          'README.md': {
+            content: 'Hello World' // Content of the Gist file
+          }
+        }
+      });
+  
+      
+      console.log('Gist created:', response.data.html_url);
+    } catch (error) {
+     
+      console.error('Error creating Gist:', error);
+    }
+  }
+
+
+
+
+
 
   const handleRemove = async (e) => {
     e.preventDefault();
@@ -49,13 +96,13 @@ function Task({ task, id, todos }) {
           className="checkbox"
           // onClick={handleMarkDone}
           onChange={() => handleMarkDone(task.id)}
-          checked={task.completed}
+           checked={task.completed}
         />
       </div>
       <div className="task-info text-slate-900 text-sm w-10/12">
         <h4 className="task-title text-lg capitalize">{task.title}</h4>
         <p className="task-description">{task.description}</p>
-        {/* <p className="task-description">{task.todos[0]}</p> */}
+        
         <div className=" italic opacity-60">
           {task?.createdAt ? (
             <p>{moment(task.createdAt).fromNow()}</p>
@@ -65,11 +112,21 @@ function Task({ task, id, todos }) {
         </div>
       </div>
       <div className="remove-task text-sm text-white">
+
+      <Tooltip title="Export as Gist">
+      <UploadIcon
+          style={{ fontSize: 30, cursor: "pointer" }}
+          size="large"
+          onClick={uploadGist}
+          className="upload-task-btn bg-blue-700 rounded-full border-2 shadow-2xl border-white p-1 m-1"
+        />
+    </Tooltip>
+      
         <DeleteIcon
           style={{ fontSize: 30, cursor: "pointer" }}
           size="large"
           onClick={handleRemove}
-          className="remove-task-btn bg-blue-700 rounded-full border-2 shadow-2xl border-white p-1"
+          className="remove-task-btn bg-blue-700 rounded-full border-2 shadow-2xl border-white p-1 m-1"
         />
       </div>
     </div>
