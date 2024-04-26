24e40a5f26f62d5d722f78ba86e43c918bc91e02
Vindhuja BK <vindhuja.bk@gmail.com>
2024-04-26 16:12:24 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,13 @@ mongoose.set('strictQuery', true);
 //middlewares
 app.use(express.json())
 app.use(cors())
+app.use(cors(
+	{
+		origin: ["https://todo-app-nine-beige-94.vercel.app/"],
+		methods: ["POST", "GET", "DELETE", "PUT"],
+		credentials: true
+	}
+));
 
 //db config
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
