5aa0b045d83690ae8f24daadd3226d35160cbe4e
Vindhuja BK <vindhuja.bk@gmail.com>
2024-04-26 15:58:32 +0530

    baseurl

--- frontend/src/Axios/axios.js
+++ frontend/src/Axios/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios"
 const instance = axios.create({
-    baseURL:"http://localhost:8000/api"
+    // baseURL:"http://localhost:8000/api"
+    baseURL:"https://todo-app-nine-beige-94.vercel.app/api"
 })
 export default instance
