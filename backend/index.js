3ff65e660ff9bc4eb5017002e1a3eb1aaa02c20f
Vindhuja BK <vindhuja.bk@gmail.com>
2024-04-27 10:43:20 +0530

    vercel header added

--- backend/index.js
+++ backend/index.js
@@ -14,10 +14,7 @@ mongoose.set('strictQuery', true);
 
 //middlewares
 app.use(express.json())
-app.use(cors({
-  origin: 'https://todo-oo15n40ou-vindhujas-projects.vercel.app',
-  credentials: true
-}));
+app.use(cors());
 
 //db config
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
